9cd7bb2ca9fc4623c96b868ad1badb7b29d1b4e2
Mohammad alfagih <mmalfagih@gmail.com>
2025-11-22 12:45:44 +0300

    feat: add User model with JWT authentication and password hashing

---
+++ src/models/User.ts
@@ -0,0 +1,45 @@
+import { Schema, model, Document } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+
+const {hash, compare} = bcrypt
+const { sign } = jwt
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  isDriver: boolean;
+  generateJWT(): Promise<string>;
+  comparePassword(enteredpassword: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema ({
+  name : {type : String, required:true},
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isDriver :{type: Boolean, default:false}
+})
+
+UserSchema.pre("save", async function (this: IUser) {
+  if (this.isModified("password")) {
+    this.password = await hash(this.password, 10);
+  }
+});
+
+UserSchema.methods.generateJWT = async function () {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+  return sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+UserSchema.methods.comparePassword = async function (enteredpassword: string) {
+  return await compare(enteredpassword, this.password);
+};
+
+const User = model<IUser>("User", UserSchema);
+
+export default User;
