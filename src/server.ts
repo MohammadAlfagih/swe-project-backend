07b9f1a18db14716b53e556af940ef7a22aefbf3
Mohammad alfagih <mmalfagih@gmail.com>
2025-11-22 12:54:07 +0300

    add JWT_SECRET to environment configuration and update server routes for user registration and login

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import  express  from "express";
 import  * as dotenv from "dotenv";
-
+import { registerUser, loginUser } from "./controllers/userControllers"
 dotenv.config()
 const PORT = process.env.PORT
 
 const app = express()
-
-app.get("/", (req,res) => {
-  res.send("hello from server")
-})
-
+const router = express.Router();
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`)
 })
