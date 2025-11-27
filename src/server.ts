c42ada0d373ce6585c900d923b86f4a6fd82a843
Mohammad alfagih <mmalfagih@gmail.com>
2025-11-27 11:25:35 +0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import  express  from "express";
 import  * as dotenv from "dotenv";
+import cors from "cors"
 import { registerUser, loginUser,getUserProfile } from "./controllers/userControllers"
 import { protect } from "./middleware/authMiddleware"; 
 import connectDB from "./config/DB";
@@ -10,8 +11,13 @@ connectDB();
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+
+app.use(cors())
 app.use(express.json());
 const router = express.Router();
+
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
