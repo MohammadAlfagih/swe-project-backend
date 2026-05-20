e91315f9009cd57d9cb3ad44611f7a44373ef7b1
Mohammad alfagih <mmalfagih@gmail.com>
2026-05-20 19:51:56 +0300

    feat: add security and testing enhancements
    - Added express-rate-limit and helmet for improved security.
    - Implemented rate limiting to prevent DoS and DDoS attacks.
    - Updated package.json to include jest and supertest for testing.
    - Created jest.config.js for configuring Jest with TypeScript support.
    - Modified server.ts to integrate security middleware and request limiting.

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import authRoutes from "./routes/authRoutes";
 import rideRoutes from "./routes/rideRoutes";
 import userRoutes from "./routes/userRoutes";
 import connectDB from "./config/DB";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 dotenv.config()
 
 connectDB();
@@ -12,6 +14,30 @@ connectDB();
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+
+//-------------------------------------
+// Extra Layer of Protection (Security)
+//-------------------------------------
+
+
+//This is 15 Middleware to make  http response headers 
+//It hides headers to help obscure the technology stack
+//enforces X-Frame-Options to prevent Clickjacking
+// and implements basic Content-Security-Policy to mitigate XSS attacks
+app.use(helmet())
+
+
+// This is Api limiter based on IP, each ip has a max of 100 requests each 15 minutes
+// it help preventing DoS and DDoS attacks
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message:{message: "Too many requests from this ip, Try agian after 15 minutes"},
+  standardHeaders: true,legacyHeaders: false,
+})
+
+app.use('/api/',apiLimiter)
 
 
 app.use(cors())
